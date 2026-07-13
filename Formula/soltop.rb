ee3b6bd1d2498eb3fae787b885a6164f9ae79701
charsyam <charsyam@gmail.com>
2026-07-13 11:19:57 +0900

    soltop 0.7.3

--- Formula/soltop.rb
+++ Formula/soltop.rb
@@ -1,8 +1,8 @@
 class Soltop < Formula
   desc "Apple Silicon GPU/CPU/power monitor (no sudo, no powermetrics)"
   homepage "https://github.com/charsyam/soltop"
-  url "https://github.com/charsyam/soltop/archive/refs/tags/v0.7.2.tar.gz"
-  sha256 "c563dedd1e990b3f7d8899f975bedb5758483a826a48f692e36a84fd541f7fc7"
+  url "https://github.com/charsyam/soltop/archive/refs/tags/v0.7.3.tar.gz"
+  sha256 "d343baf17eb272fbbdbf1d9a8fb522a013c422fd2bb5b8bf0469393a212a9836"
   license "MIT"
   head "https://github.com/charsyam/soltop.git", branch: "main"
 
